f3235532ce2da1aeae1d661383676148fa79729b
Developer <dev@example.com>
2025-12-22 09:25:43 +0100

    feat(transaction): update UpdateTransaction DTO to require Type and Category fields
    - Made Type and Category properties required for better validation
    - Made Description property optional to allow for flexibility

--- Expenses.API/Domain/Transaction/Dto/UpdateTransaction.cs
+++ Expenses.API/Domain/Transaction/Dto/UpdateTransaction.cs
@@ -1,7 +1,8 @@
-﻿namespace Expenses.API.Domain.Transaction.Dto {
+namespace Expenses.API.Domain.Transaction.Dto {
     public class UpdateTransaction {
-        public string Type { get; set; } // e.g., "Income" or "Expense"
-        public string Description { get; set; }
+        public required string Type { get; set; } // e.g., "Income" or "Expense"
+        public required string Category { get; set; }
+        public  string? Description { get; set; }
 
     }
 }
